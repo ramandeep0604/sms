87d086971030eac891c0c4a3750cd0f7fcc0a08a
ramandeep0604 <ramandeepkaur4619@gmail.com>
2026-04-23 11:52:17 +0530

    feat:user module

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
 import express from "express";
-
+import dotenv from "dotenv"
+import connectDb from "./db/config.js";
 const app=express();
+
+dotenv.config()
+
+connectDb()
 
 app.get('/health',(req,res)=>{
     res.send("health is ok")
---
+++ server/db/config.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose"
+const connectDb=async()=>{
+    try {
+       const connection= await mongoose.connect(process.env.MONGODB_URI)
+       console.log("mongodb connected");
+       
+    } catch (error) {
+     console.log(error)   
+    }
+}
+export default connectDb
