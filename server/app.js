b688d6119fc1a27f8ac1ceb5ac41a0f6926145ce
ramandeep0604 <ramandeepkaur4619@gmail.com>
2026-04-23 10:38:35 +0530

    feat:added health check route

--- server/app.js
+++ server/app.js
@@ -2,7 +2,10 @@ import express from "express";
 
 const app=express();
 
+app.get('/health',(req,res)=>{
+    res.send("health is ok")
 
+})
 
 app.listen(3000,()=>{
 console.log("server is running");
